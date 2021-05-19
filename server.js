ffcbb461451b9354ad40b8c7d2b713e9b63fcc8b
Oussamaghzaeil <gazeiloussama15@gmail.com>
2021-05-19 23:38:33 +0100

    second

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 //serve only the static files form the dist directory
-app.use(express.static('./dist/Front/'));
+app.use(express.static('./dist/Front/src/app/login'));
 
 app.get('/"',(req, res) =>
-    res.sendFile('index.html',{root: 'dist/Front'}),
+    res.sendFile('login.component.html',{root: 'dist/Front/src/app/login'}),
 );
 
 // start the app by listening on the default heroku port
