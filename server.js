f2fe1a6efbeb1e477560206bd3c0a9ba9a39714b
Oussamaghzaeil <gazeiloussama15@gmail.com>
2021-05-20 01:38:28 +0100

    e

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
+//Initial express server
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+
 const app = express();
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    'Content-Security-Policy-Report-Only',
-    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
-  );
-  next();
-});
+//serve only the static files form the dist directory
+app.use(express.static('./Front/src/'));
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname)));
+app.get('/"',(req, res) =>
+    res.sendFile('index.html',{root: 'Front/src'}),
+);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+// start the app by listening on the default heroku port
+app.listen(process.env.PORT || 8080);
 
-const server = app.listen(process.env.PORT || 8080, () => {
-  const { port } = server.address();
-  console.log(`Server running on PORT ${port}`);
-});
