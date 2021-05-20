4d0c6edce3808fc4153d42f7590e040e5edc22e9
Oussamaghzaeil <gazeiloussama15@gmail.com>
2021-05-20 01:27:29 +0100

    d

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-const server = app.listen(process.env.PORT || 5500, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
   const { port } = server.address();
   console.log(`Server running on PORT ${port}`);
 });
